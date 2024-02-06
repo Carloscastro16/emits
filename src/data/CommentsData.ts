bb27bb318ea56244c53d276b580db5f3e4088bde
ccastro1016 <andycastro.001016@gmail.com>
2024-02-06 17:37:21 -0500

    Initial commit

---
+++ src/data/CommentsData.ts
@@ -0,0 +1,33 @@
+import type { IComment } from "@/interfaces/IComment";
+
+const CommentsData: IComment[] = [
+    {
+        id: 1,
+        title: 'Comentario 1',
+        author: 'Carlos',
+        body: 'Lorem ipsom dolor sit amet, consectetur adipiscing elit',
+        date: new Date(),
+    },
+    {
+        id: 2,
+        title: 'Comentario 3',
+        author: 'Ileana',
+        body: 'Lorem ipsom dolor sit amet, consectetur adipiscing elit',
+        date: new Date(),
+    },
+    {
+        id: 3,
+        title: 'Comentario 3',
+        author: 'Jose',
+        body: 'Lorem ipsom dolor sit amet, consectetur adipiscing elit',
+        date: new Date(),
+    },
+    {
+        id: 4,
+        title: 'Comentario 4',
+        author: 'Camila',
+        body: 'Lorem ipsom dolor sit amet, consectetur adipiscing elit',
+        date: new Date(),
+    },
+
+]
